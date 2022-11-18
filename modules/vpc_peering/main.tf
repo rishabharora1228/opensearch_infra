8fe569eae5f2d6ae04c64498d041eedcebf662e4
rishabharora1228 <114339911+rishabharora1228@users.noreply.github.com>
2022-11-18 20:42:26 +0530

    Update main.tf

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -32,3 +32,8 @@ resource "aws_route" "peering_2" {
   destination_cidr_block    = var.destination_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc.id
 }
+resource "aws_route" "peering_3" {
+  route_table_id            = "rtb-0835ec3d4aa844e52"
+  destination_cidr_block    = var.destination_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc.id
+}
