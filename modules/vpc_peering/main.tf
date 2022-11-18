8c83a6cd36e6660ad1e4e47c7d98da39ba5aaf5a
rishabharora1228 <114339911+rishabharora1228@users.noreply.github.com>
2022-11-18 10:41:25 +0530

    Update main.tf

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -13,22 +13,19 @@ resource "aws_vpc_peering_connection" "vpc" {
   peer_owner_id = data.aws_caller_identity.owner.account_id
   peer_vpc_id   = data.aws_vpc.vpc.id
   vpc_id        = var.vpc_id
-#   auto_accept   = true
-#     accepter {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  auto_accept   = true
+    accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   requester {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 }
 resource "aws_route" "peering_1" {
   route_table_id            = var.peering1_id
   destination_cidr_block    = data.aws_vpc.vpc.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc.id
-      accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
 }
 resource "aws_route" "peering_2" {
   route_table_id            = data.aws_route_table.routetable.id
