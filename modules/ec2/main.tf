58680873bbf6785a0d8d43f0c7af6dfcdc260ab2
rishabharora1228 <114339911+rishabharora1228@users.noreply.github.com>
2022-11-19 12:42:30 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami                    = var.ami
-  instance_type          = "t2.large"
+  instance_type          = var.instance_type
   tags                   = var.ec2_tags
   key_name               = var.key_id
   vpc_security_group_ids = [var.security_group]
